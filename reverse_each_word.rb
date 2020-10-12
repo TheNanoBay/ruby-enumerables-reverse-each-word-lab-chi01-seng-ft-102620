7adfc0f03c1d371048990b5e98d8ec61adc43b21
Mehdi A Noorani <noorani.mehdi@outlook.com>
2020-10-12 22:11:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,14 @@
 
 def reverse_each_word (my_sentence)
-  my_sentence.split.map {|word| word.reverse}.join(" ")
   
-end 
+  my_sentence.split.collect do |word| 
+    
+  
+  
+end
+  
+  
+  
+ #my_sentence.split.map {|word| word.reverse}.join(" ")
+  
+ 
